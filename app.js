d549f723693fde8320abf3fd18de2a0305bcbe9d
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-31 16:53:35 -0700

    added code for app.js data.js and index.html to allow chart.js to work

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-// import functions and grab DOM elements
+import { customers } from './data.js';
 
-// let state
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
