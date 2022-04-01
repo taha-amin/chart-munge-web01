99b105be991594b64a56f537d408ce7428710e5d
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-01 09:51:00 -0700

    created app2.js and in data-utils.js file wrote func for 2nd assignment (line chart showing number of customers by cool factor). In index.html wrote new canvas id on line 26 for chart 2 and new script on line 36

---
+++ app2.js
@@ -0,0 +1,27 @@
+import { customers } from './data.js';
+import { makeCoolFactorCountMap } from './data-utils.js';
+
+const countMap2 = makeCoolFactorCountMap(customers);
+
+const labels = Object.keys(countMap2);
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: '# of customers by cool factor',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: Object.values(countMap2),
+    }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart2'),
+    config
+);
--- data-utils.js
+++ data-utils.js
@@ -15,3 +15,19 @@ export function makePurchaseFrequencyCountMap() {
 
     return countMap;
 }
+
+export function makeCoolFactorCountMap() {
+    const countMap2 = {};
+
+    for (let customer of customers) {
+        const coolFactor = customer.cool_factor;
+
+        if (countMap2[coolFactor]) {
+            countMap2[coolFactor]++;
+        } else {
+            countMap2[coolFactor] = 1;
+        }
+    }
+
+    return countMap2;
+}
